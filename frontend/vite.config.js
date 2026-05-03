1124dde5f98ab7a5e4fe11449a75e8f0866bc11f
Khoa Zr <duongcongkhoa171006@gmail.com>
2026-05-04 05:41:33 +0700

    fronend: cung cấp quyền cho vite truy cập domain

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: [
+      'one-line-solver-1.onrender.com'
+    ]
+  }
 })
